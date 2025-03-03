31d5f70e794dfabaf229f0a514c5a8b31c341b2f
pleibrandt <leibrandt16@gmail.com>
2025-03-03 16:21:41 +0100

    Koniec zadania 4

---
+++ Zadanie_4/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+using System;
+
+namespace Zadanie_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b;
+            Console.Write("Podaj bok a: ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Podaj bok b: ");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Obwód wynosi: {0}", a+a+b+b);
+            Console.WriteLine("Pole wynosi: {0}", a*b);
+            Console.ReadKey(true);
+        }
+    }
+}
