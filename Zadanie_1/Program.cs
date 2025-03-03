935aca353bb2c4d98be3a1dba22fbcad1531f9e9
pleibrandt <leibrandt16@gmail.com>
2025-03-03 15:33:54 +0100

    Koniec zadania 1

---
+++ Zadanie_1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Zadanie_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Witaj świecie!");
+            Console.ReadKey(true);
+        }
+    }
+}
