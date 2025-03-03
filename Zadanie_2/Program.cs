9bc4c27421a35d478c219cb94b827393e3e4435f
pleibrandt <leibrandt16@gmail.com>
2025-03-03 16:05:38 +0100

    Koniec zadania 2

---
+++ Zadanie_2/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+
+namespace Zadanie_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a;
+            Console.Write("Podaj imie: ");
+            a = Console.ReadLine();
+            Console.WriteLine("Witaj {0}!", a);
+            Console.ReadKey(true);
+        }
+    }
+}
