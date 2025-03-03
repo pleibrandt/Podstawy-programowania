589e9af11586a009e1d2ea08014367eae2fb33fc
pleibrandt <leibrandt16@gmail.com>
2025-03-03 16:14:27 +0100

    Koniec zadania 3

---
+++ Zadanie_3/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+using System;
+
+namespace Zadanie_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+
+            Console.Write("Podaj liczbe: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Szescian liczby wynosi: {0}", a*a*a);
+            Console.ReadKey(true);
+        }
+    }
+}
