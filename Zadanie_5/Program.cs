58a7d91233553663ba495590d019eae07714fb28
pleibrandt <leibrandt16@gmail.com>
2025-03-03 16:33:03 +0100

    Koniec zadania 5

---
+++ Zadanie_5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace Zadanie_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double h, r;
+
+            Console.Write("Podaj wysokość: ");
+            h = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Podaj promien kola: ");
+            r = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Objetosc wynosi: {0}", 1.0/3*Math.PI*r*r*h);
+            Console.WriteLine("Pole calkowite wynosi: {0}", Math.PI*r*r+Math.PI*r*Math.Sqrt(h*h+r*r));
+            Console.ReadKey(true);
+        }
+    }
+}
