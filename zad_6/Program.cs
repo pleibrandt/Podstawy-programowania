dfa96905d616932cf0d50eaf4ac717849b11edf5
pleibrandt <leibrandt16@gmail.com>
2025-03-06 21:02:49 +0100

    Zadanie 6

---
+++ zad_6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Reflection;
+
+
+namespace zad_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b;
+            Console.Write("Podaj dwie liczby: ");
+            a = Convert.ToDouble(Console.ReadLine());
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Suma wynosi: {0}, różnica wynosi: {1}, iloczyn wynosi: {2}, iloraz wynosi: {3}",a+b,a-b,a*b,a/b);
+            Console.ReadKey(true);
+        }
+    }
+}
