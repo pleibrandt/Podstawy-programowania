9b824dce738361444443e8cd8f9cbed074c2d88f
pleibrandt <leibrandt16@gmail.com>
2025-03-07 14:06:17 +0100

    Koniec zadania 1

---
+++ Zadanie_1_iw/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_1_iw
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.Write("Podaj liczbe: ");
+            a = int.Parse(Console.ReadLine());
+            if (a % 2 == 0)
+            {
+                Console.WriteLine("Liczba jest parzysta.");
+            }
+            else
+            {
+                Console.WriteLine("Liczba jest nie parzysta.");
+            }
+            Console.ReadKey(true);
+        }
+    }
+}
