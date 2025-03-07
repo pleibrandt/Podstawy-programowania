8733a75df2f0ae536a82d2d660b2ffb645dae90f
pleibrandt <leibrandt16@gmail.com>
2025-03-07 13:51:43 +0100

    Zadanie 6 i 7

---
+++ Zadanie_7/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+
+namespace Zadanie_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("Podaj dwie liczby: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Iloraz wynosi: {0}, a reszta z dzielenia wynosi: {1}",a/b, a%b);
+        }
+    }
+}
