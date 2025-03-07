1ad937ee855875addf50af9cdc662488f281f42e
pleibrandt <leibrandt16@gmail.com>
2025-03-07 23:52:17 +0100

    Koniec zadania 2

---
+++ Zadanie_2_iw/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+
+namespace Zadanie_2_iw
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.Write("Podaj liczbe: ");
+            a = int.Parse(Console.ReadLine());
+            if (a > 0)
+            {
+                Console.WriteLine("Liczba jest dodatnia.");
+            }
+            else if(a < 0)
+            {
+                Console.WriteLine("Liczba jest ujemna.");
+            }
+            else
+            {
+                Console.Write("Liczba jest równa 0.");
+            }
+                Console.ReadKey(true);
+        }
+    }
+}
