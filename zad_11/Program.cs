2597ab392629ec82fd20125ebc220bcb375b1a0b
pleibrandt <leibrandt16@gmail.com>
2025-03-10 22:14:29 +0100

    zad 11/t1

---
+++ zad_11/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+using System;
+
+namespace zad_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, h, pole_c, obj;
+            Console.Write("Podaj dlugosc boku: ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Podaj dlugosc boku: ");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Podaj dlugosc wysokosci: ");
+            h = Convert.ToDouble(Console.ReadLine());
+            pole_c = a * b * 2 + a * h * 2 + b * h * 2;
+            obj = a * b * h;
+            Console.WriteLine("Pole calkowite prostopadloscianu wynosi {0}, a jego objetosc wynosi {1}", pole_c, obj);
+            Console.ReadKey(true);
+
+        }
+    }
+}
