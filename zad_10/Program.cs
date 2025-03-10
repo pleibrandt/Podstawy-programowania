09a15335ffae70a14b9e46c73e071588422fd524
pleibrandt <leibrandt16@gmail.com>
2025-03-10 22:07:44 +0100

    zad 10/t1

---
+++ zad_10/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+using System;
+
+namespace zad_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r, obw, pole;
+            Console.Write("Podaj dlugosc promienia: ");
+            r = Convert.ToDouble(Console.ReadLine());
+            obw = 2 * Math.PI * r;
+            pole = Math.PI * r * r;
+            Console.WriteLine("Obwod kola wynosi {0}, a pole kola wynosi {1}", obw, pole);
+            Console.ReadKey(true);
+
+        }
+    }
+}
