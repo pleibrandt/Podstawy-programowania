36071366bf77d93cc56ad47afb53eb8f10b2794b
pleibrandt <leibrandt16@gmail.com>
2025-03-10 22:02:32 +0100

    Zadanie 9/t1

---
+++ zad_9/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+using System;
+
+namespace zad_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, sum;
+            Console.Write("Podaj liczbe: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Podaj liczbe: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            sum = a * a + b * b;
+            Console.WriteLine("Suma kwadratow wynosi: {0}", sum);
+            Console.ReadKey(true);
+
+        }
+    }
+}
