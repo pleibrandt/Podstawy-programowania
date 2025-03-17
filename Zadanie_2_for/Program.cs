f40b0c47c4964cdfc08984441a35ca182ce98652
pleibrandt <leibrandt16@gmail.com>
2025-03-17 15:14:36 +0100

    zad 2/t3

---
+++ Zadanie_2_for/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_2_for
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n,pot=2;
+            Console.WriteLine("Podaj potege: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            for(int i=1; i<n; i++)
+            {
+                pot = pot * 2;
+            }
+            Console.WriteLine("2^{0}: {1}",n,pot);
+            Console.ReadKey(true);
+        }
+    }
+}
