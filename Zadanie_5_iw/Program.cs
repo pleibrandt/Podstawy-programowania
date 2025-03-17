a1e545a1488df6bb9580d2bc0f5cee46c2700131
pleibrandt <leibrandt16@gmail.com>
2025-03-17 14:03:50 +0100

    zad 5/t2

---
+++ Zadanie_5_iw/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_5_iw
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rok;
+            Console.WriteLine("Podaj rok: ");
+            rok = Convert.ToInt32(Console.ReadLine());
+            if(((rok%4==0) && (rok%100!=0)) || (rok%400==0))
+                Console.WriteLine("Rok jest przystepny");
+            else
+                Console.WriteLine("Rok nie jest przystepny");
+        }
+    }
+}
