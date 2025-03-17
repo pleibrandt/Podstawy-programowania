f27629de9e92cf31e79c3cd6671cfe3dc78cec5b
pleibrandt <leibrandt16@gmail.com>
2025-03-17 13:46:36 +0100

    zad 3/t2

---
+++ Zadanie_3_ie/Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+using System;
+
+namespace Zadanie_3_ie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c, najw, najm;
+            Console.WriteLine("Podaj 3 liczby: ");
+            a = Convert.ToDouble(Console.ReadLine());
+            b = Convert.ToDouble(Console.ReadLine());
+            c = Convert.ToDouble(Console.ReadLine());
+
+            if (a > b && a > c)
+                najw = a;
+            else if (b > a && b > c)
+                najw = b;
+            else
+                najw = c;
+
+            if (a < b && a < c)
+                najm = a;
+            else if (b < a && b < c)
+                najm = b;
+            else
+                najm = c;
+
+            Console.WriteLine("Najmniejsza liczba jest {0}, a najwieksza jest {1}",najm, najw);
+
+        }
+    }
+}
