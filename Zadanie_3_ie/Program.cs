33dcf84fe26c759a62655a9ec42d9621d0dacb8d
pleibrandt <leibrandt16@gmail.com>
2025-03-17 13:58:36 +0100

    zad 4/t2

--- Zadanie_3_ie/Program.cs
+++ Zadanie_3_ie/Program.cs
@@ -14,16 +14,16 @@ namespace Zadanie_3_ie
             b = Convert.ToDouble(Console.ReadLine());
             c = Convert.ToDouble(Console.ReadLine());
 
-            if (a > b && a > c)
+            if (a >= b && a >= c)
                 najw = a;
-            else if (b > a && b > c)
+            else if (b >= a && b >= c)
                 najw = b;
             else
                 najw = c;
 
-            if (a < b && a < c)
+            if (a <= b && a <= c)
                 najm = a;
-            else if (b < a && b < c)
+            else if (b <= a && b <= c)
                 najm = b;
             else
                 najm = c;
---
+++ Zadanie_4_iw/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_4_iw
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c, d, e, najm, najw;
+            Console.WriteLine("Podaj 5 liczb: ");
+
+            a = Convert.ToDouble(Console.ReadLine());
+            b = Convert.ToDouble(Console.ReadLine());
+            c = Convert.ToDouble(Console.ReadLine());
+            d = Convert.ToDouble(Console.ReadLine());
+            e = Convert.ToDouble(Console.ReadLine());
+
+            najm = a;
+            if (b < najm)
+                najm = b;
+            if (c < najm)
+                najm = c;
+            if (d < najm)
+                najm = d;
+            if (e < najm)
+                najm = e;
+
+            najw = a;
+            if (b > najw)
+                najw = b;
+            if (c > najw)
+                najw = c;
+            if (d > najw)
+                najw = d;
+            if (e > najw)
+                najw = e;
+
+            Console.WriteLine("Najmniejsza liczba to {0}, a najwieksza to {1}",najm,najw);
+        }
+    }
+}
