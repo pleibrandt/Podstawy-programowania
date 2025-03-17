8e4f0215df85c29c82460f9459162aaa5c054d8f
pleibrandt <leibrandt16@gmail.com>
2025-03-17 15:06:52 +0100

    zad 1/t3

---
+++ Zadanie_1_for/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_1_for
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i=11; i<=33; i++)
+            {
+                Console.Write("{0}, ", i);
+            }
+            Console.ReadKey(true);
+        }
+    }
+}
