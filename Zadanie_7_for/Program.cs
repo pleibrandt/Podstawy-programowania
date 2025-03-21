38996555b4e719171452b79c87d3ec911c0383a2
pleibrandt <leibrandt16@gmail.com>
2025-03-21 13:42:52 +0100

    zad 7/t3

---
+++ Zadanie_7_for/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_7_for
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+                if (i % 13 == 0)
+                    Console.Write("{0}, ", i);
+            Console.ReadKey(true);
+        }
+    }
+}
