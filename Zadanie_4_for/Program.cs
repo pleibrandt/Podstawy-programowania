3fd11ddcbfd89425fd07bdd5556ac4dcaff51784
pleibrandt <leibrandt16@gmail.com>
2025-03-21 13:22:35 +0100

    zad 4/t3

---
+++ Zadanie_4_for/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_4_for
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, k;
+            Console.WriteLine("Podaj dwie liczby naturalne: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            k = Convert.ToInt32(Console.ReadLine());
+
+            for(int i=0; i<n; i++)
+            {
+                for (int j = 0; j < k; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
+            Console.ReadKey(true);
+        }
+    }
+}
