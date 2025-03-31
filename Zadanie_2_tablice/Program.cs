7787061d0049f9575ffc6bbac68cdf4d1a1b5ac3
pleibrandt <leibrandt16@gmail.com>
2025-03-31 14:14:50 +0200

    zad 2/t6

---
+++ Zadanie_2_tablice/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_2_tablice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, i = 0;
+            int[] tab = new int[1000];
+            Console.Write("Ile liczb wpisac: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            for (i = 0; i < n; i++)
+            {
+                tab[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Elementy tablicy w odwrotnej kolejnosci to: ");
+            for (i = n-1; i >= 0; i--)
+            {
+                Console.Write("{0}, ", tab[i]);
+            }
+            Console.ReadKey(true);
+        }
+    }
+}
