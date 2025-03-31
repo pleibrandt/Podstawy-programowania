19776313e9a624a7033a180d1fe201b43bc7951d
pleibrandt <leibrandt16@gmail.com>
2025-03-31 14:08:19 +0200

    zad 1/t6

---
+++ Zadanie_1_tablice/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_1_tablice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n,i=0; 
+            int[] tab = new int[1000];
+            Console.WriteLine("Ile liczb wpisac: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            for (i = 0; i < n; i++)
+            {
+                tab[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Elementy tablicy to: ");
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("{0}, ", tab[i]);
+            }
+            Console.ReadKey(true);
+        }
+    }
+}
