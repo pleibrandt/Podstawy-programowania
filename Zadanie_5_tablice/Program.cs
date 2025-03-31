7f9fb6173c272c9b4b3ae71621c8f33d3c43584f
pleibrandt <leibrandt16@gmail.com>
2025-03-31 14:44:44 +0200

    zad 5/t6

---
+++ Zadanie_5_tablice/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_5_tablice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, a=0;
+            int[] bin = new int[1000];
+            Console.Write("Podaj liczbe: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            while(n>=1)
+            {
+                bin[a] = n % 2;
+                a++;
+                n = n / 2;
+            }
+            Console.WriteLine("Binarna wyniesie: ");
+            for(int i=a-1; i>=0; i--)
+            {
+                Console.Write("{0}", bin[i]);
+            }
+
+        }
+    }
+}
---
+++ Zadanie_5_tablice/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("Zadanie_5_tablice")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Zadanie_5_tablice")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("ca236c23-35db-45f5-847d-f861f809ee16")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Poprawka
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
