bc596a1c52fbf9d603256043c3d246483119678d
pleibrandt <leibrandt16@gmail.com>
2025-03-31 14:53:13 +0200

    zadanie 6/t6

---
+++ Zadanie_6_tablice/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_6_tablice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] dni_tygodnia = new string[] {"poniedzialek","wtorek","sroda","czwartek","piatek","sobota","niedziela"};
+            foreach(string c in dni_tygodnia)
+            {
+                Console.WriteLine(c);
+            }
+        }
+    }
+}
