80889de9c19ce05d02d0a6fe98a9558fd637987a
pleibrandt <leibrandt16@gmail.com>
2025-04-04 14:04:12 +0200

    zad 5/t4

---
+++ Zadanie_5_while/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_5_while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x;
+            do
+            {
+                Console.Write("Podaj liczbe dodatnia: ");
+                x = Convert.ToDouble(Console.ReadLine());
+            } while (x <= 0);
+            Console.WriteLine("Liczba to: {0}",x);
+            Console.ReadKey(true);
+        }
+    }
+}
