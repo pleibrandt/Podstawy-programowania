03e67c130b5a5e83562fc6f24c6b6e5fc7174b29
pleibrandt <leibrandt16@gmail.com>
2025-04-04 13:58:29 +0200

    zad 4/t4

---
+++ Zadanie_4_while/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_4_while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, k,a=0,b=0;
+            Console.Write("Podaj n: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Podaj k: ");
+            k = Convert.ToInt32(Console.ReadLine());
+
+            while(a<n)
+            {
+                while (b < k)
+                {
+                    Console.Write("*");
+                    b++;
+                }
+                Console.WriteLine("");
+                a++;
+                b = 0;
+            }
+            Console.ReadKey(true);
+        }
+    }
+}
