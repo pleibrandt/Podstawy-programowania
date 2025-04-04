33fe900f81250d9ff28c84b735a003196d75ca70
pleibrandt <leibrandt16@gmail.com>
2025-04-04 13:52:10 +0200

    zad 3/t4

---
+++ Zadanie_3_while/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_3_while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, sum=0;
+            Console.Write("Podaj a: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            do
+            {
+                Console.Write("Podaj b: ");
+                b = Convert.ToInt32(Console.ReadLine());
+            } while (b < a);
+
+            do
+            {
+                sum = sum + a;
+                a++;
+            } while (a <= b);
+            Console.WriteLine("Suma elementow wynosi: {0}",sum);
+            Console.ReadKey(true);
+        }
+    }
+}
