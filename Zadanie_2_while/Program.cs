8c759b3092e2aec781224a97883fb14c673e37f5
pleibrandt <leibrandt16@gmail.com>
2025-04-04 13:46:24 +0200

    zad 2/t4

---
+++ Zadanie_2_while/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_2_while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n,a=1;
+            Console.WriteLine("Podaj n: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            while (a <= n)
+            {
+                Console.WriteLine("a({0})={1}",a,2*a-1);
+                a++;
+            }
+            Console.ReadKey(true);
+        }
+    }
+}
