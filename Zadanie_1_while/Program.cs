d1d58576b78b0cf25c3d45db0bf7aa1cb63b6d15
pleibrandt <leibrandt16@gmail.com>
2025-04-04 13:38:51 +0200

    zad 1/t4

---
+++ Zadanie_1_while/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_1_while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint a, b;
+            Console.Write("Podaj poczatek przedzialu: ");
+            a = Convert.ToUInt32(Console.ReadLine());
+            do
+            {
+                Console.Write("Podaj koniec przedzialu (wiekszy od poczatku): ");
+                b = Convert.ToUInt32(Console.ReadLine());
+            } while (b < a);
+
+            do
+            {
+                if (a % 2 == 0)
+                    Console.Write("{0}  ", a);
+                a++;
+            } while (a <= b);
+            Console.ReadKey(true);
+        }
+    }
+}
