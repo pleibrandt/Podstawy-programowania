7211359238a8e362a9ab39d7de4230b95e174c7a
pleibrandt <leibrandt16@gmail.com>
2025-04-08 16:45:12 +0200

    zad 10/t3

---
+++ zad_10_for/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad_10_for
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ile;
+            Console.Write("Podaj ile poda liczb podzielnych przez 7: ");
+            ile = Convert.ToInt32(Console.ReadLine());
+
+            for(int i=0; i<ile; i++)
+            {
+                Console.Write("{0}, ", i * 7);
+            }
+            Console.ReadKey(true);
+        }
+    }
+}
