0cebd340170e9994634ddefa5a3ee5fcf0f2fc58
pleibrandt <leibrandt16@gmail.com>
2025-04-08 16:59:59 +0200

    zad 12/t3

---
+++ zad_12_for/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad_12_for
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int h,dod;
+            Console.Write("Podaj wysokosc piramidy: ");
+            h = Convert.ToInt32(Console.ReadLine());
+            for(int i=1; i<=h; i++)
+            {
+                dod = i;
+                for(int j=0; j<i; j++)
+                {
+                    Console.Write("{0} ", dod);
+                    dod = dod + i;
+                }
+                Console.WriteLine("");
+            }
+            Console.ReadKey(true);
+        }
+    }
+}
