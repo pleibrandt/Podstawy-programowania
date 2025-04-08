1af1107b539e5483c21b80dc0a8932bf4e76e7b2
pleibrandt <leibrandt16@gmail.com>
2025-04-08 16:54:35 +0200

    zad 11/t3

---
+++ zad_11_for/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad_11_for
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n,suma=0, niep=1;
+            Console.Write("Podaj n: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            for(int i=0; i<n; i++)
+            {
+                suma = suma + niep;
+                niep = niep + 2;
+            }
+            Console.WriteLine("Suma początkowych liczby nieparzystych do n wynosi: {0}",suma);
+        }
+    }
+}
