6629696ab623b64c3202a3e1949b5e6dfd9371e9
pleibrandt <leibrandt16@gmail.com>
2025-04-09 22:04:03 +0200

    zad 14/t3

---
+++ zad_14_for/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad_14_for
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, m, l_dwo;
+            Console.Write("Podaj poczatek przedzialu: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Podaj koniec przedzialu: ");
+            m = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Znalezione trójki: ");
+            for(int i=n; i<=m; i++)
+            {
+                for(int j=n; j<=m; j++)
+                {
+                    for(int k=n; k<=m; k++)
+                    {
+                        if ((Math.Sqrt(i * i + j * j) == k) && (i < j))
+                            Console.WriteLine("{0}, {1}, {2}",i,j,k);
+                    }
+                }
+            }
+        }
+    }
+}
