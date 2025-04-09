7d07faed3a223bd152743ad30fda5125b6b3deee
pleibrandt <leibrandt16@gmail.com>
2025-04-09 22:11:23 +0200

    zad 7/t4

---
+++ zad_7_while/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad_7_while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, i=1, silnia=1;
+            Console.Write("Podaj n: ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            while(i<n+1)
+            {
+                silnia = silnia * i;
+                i++;
+            }
+            Console.WriteLine("{0}! = {1}",n,silnia);
+            Console.ReadKey(true);
+        }
+    }
+}
