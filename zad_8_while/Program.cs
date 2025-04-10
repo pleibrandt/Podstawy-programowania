ef245d83c06cc32c49ea0c4c29152736a0659c29
pleibrandt <leibrandt16@gmail.com>
2025-04-10 23:27:22 +0200

    zad 8/t4

---
+++ zad_8_while/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad_8_while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, n=2, dziel=0;
+            Console.Write("Podaj liczbe: ");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            while(n<a)
+            {
+                if (a % n == 0)
+                {
+                    dziel++;
+                }
+                n++;
+            }
+            if(dziel==0)
+                Console.WriteLine("Liczba jest pierwsza");
+            else
+                Console.WriteLine("Liczba nie jest pierwsza");
+            Console.ReadKey(true);
+        }
+    }
+}
