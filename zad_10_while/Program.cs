1e936cb10347d28a50a565884225768947719fde
pleibrandt <leibrandt16@gmail.com>
2025-04-10 23:34:03 +0200

    zad 10/t4

---
+++ zad_10_while/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad_10_while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum=0, n;
+            do
+            {
+                Console.Write("Podaj liczbe: ");
+                n = Convert.ToInt32(Console.ReadLine());
+                sum = sum + n;
+            } while (n != 0);
+            Console.WriteLine("Suma liczb wynosi: {0}",sum);
+            Console.ReadKey(true);
+        }
+    }
+}
