093a3996f7083275f9705bf3d08348299ea99bb3
pleibrandt <leibrandt16@gmail.com>
2025-04-14 13:29:15 +0200

    zad 1/t7

---
+++ Zadanie_1_funkcje/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_1_funkcje
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.Write("Podaj a: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Podaj b: ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Suma wynosi: {0}",suma(a,b));
+        }
+
+        static int suma(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
