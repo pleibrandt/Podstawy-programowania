0acf21d9e15001f785648c71920844582d00ed08
pleibrandt <leibrandt16@gmail.com>
2025-04-14 13:36:47 +0200

    zad 2/t7

---
+++ Zadanie_2_funkcje/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace Zadanie_2_funkcje
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b;
+            Console.Write("Podaj a: ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Podaj b: ");
+            b = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Maksimum z liczb {0} i {1} wynosi: {2}",a,b,max(a,b));
+            Console.ReadKey(true);
+        }
+
+        static double max(double a, double b)
+        {
+            if (a > b)
+                return a;
+            else
+                return b;
+        }
+    }
+}
---
+++ Zadanie_2_funkcje/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("Zadanie_2_funkcje")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Zadanie_2_funkcje")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("73f55b93-eca3-43c7-8099-c102f090da88")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Poprawka
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
