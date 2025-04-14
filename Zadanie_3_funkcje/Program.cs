2fd515628873adb03d6e8af860a99410fca6c9aa
pleibrandt <leibrandt16@gmail.com>
2025-04-14 13:45:31 +0200

    zad 3/t7

---
+++ Zadanie_3_funkcje/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_3_funkcje
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r;
+            Console.Write("Podaj dlugosc promienia: ");
+            r = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Pole kola wynosi: {0}",pole_kola(r));
+        }
+
+        static double pole_kola(double r)
+        {
+            return Math.PI * r * r;
+        }
+    }
+}
