7878843aa029a2db3a402e45dcaea0c96274de7c
pleibrandt <leibrandt16@gmail.com>
2025-05-12 13:38:27 +0200

    zad 4/t7

---
+++ Zadanie_4_funkcje/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_4_funkcje
+{
+    class Program
+    {
+        static int ile_cyfr(int liczba)
+        {
+            int ile = 0;
+            while(liczba > 0)
+            {
+                ile++;
+                liczba = liczba / 10;
+            }
+            return ile;
+        }
+        static void Main(string[] args)
+        {
+            int liczba;
+            Console.Write("Podaj liczbe: ");
+            liczba = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Liczba cyfr wynosi: {0}", ile_cyfr(liczba));
+            Console.ReadKey(true);
+        }
+    }
+}
