2655647c7a512c574a7d13d3ec85ae193a920c56
pleibrandt <leibrandt16@gmail.com>
2025-05-12 14:04:27 +0200

    zad 1/t8

---
+++ Zadanie_1_reku/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_1_reku
+{
+    class Program
+    {
+        static int silnia(int n)
+        {
+            if (n == 0)
+                return 1;
+            else
+                return n * silnia(n - 1);
+        }
+        static void Main(string[] args)
+        {
+            int n;
+            Console.Write("Podaj liczbe do silni: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Silnia wynosi: {0}",silnia(n));
+            Console.ReadKey(true);
+
+
+        }
+    }
+}
