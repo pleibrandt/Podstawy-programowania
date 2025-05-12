9637c0caeb7946eb57800673bef0b5656db29b95
pleibrandt <leibrandt16@gmail.com>
2025-05-12 14:00:20 +0200

    zad 6/t7

---
+++ Zadanie_6_funkcje/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_6_funkcje
+{
+    class Program
+    {
+        static void kwadrat(int a, char kon, char wyp)
+        {
+            for(int i=0; i<a; i++)
+            {
+                for (int j = 0; j < a; j++)
+                {
+                    if (i == 0 || i == a - 1)
+                    {
+                        Console.Write("{0}", kon);
+                    }
+                    else if (j == 0 || j == a - 1)
+                    {
+                        Console.Write("{0}", kon);
+                    }
+                    else
+                        Console.Write("{0}", wyp);
+                }
+                Console.WriteLine("");
+            }
+        }
+        static void Main(string[] args)
+        {
+            int a;
+            char kon, wyp;
+            Console.Write("Podaj dlugosc boku: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Podaj znak kontura: ");
+            kon = Convert.ToChar(Console.ReadLine());
+            Console.Write("Podaj znak wypelnienia: ");
+            wyp = Convert.ToChar(Console.ReadLine());
+
+            kwadrat(a, kon, wyp);
+            Console.ReadKey(true);
+        }
+    }
+}
