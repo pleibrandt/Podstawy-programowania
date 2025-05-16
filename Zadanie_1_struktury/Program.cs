c6c7c67965d5622a9aec520bd11f5329408fc231
pleibrandt <leibrandt16@gmail.com>
2025-05-16 13:35:12 +0200

    zad 1/t9

---
+++ Zadanie_1_struktury/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_1_struktury
+{
+    struct Osoba
+    {
+        public string imie, nazwisko;
+        public int rok_urodzenia;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Osoba o;
+            Console.Write("Podaj imie: ");
+            o.imie = Convert.ToString(Console.ReadLine());
+            Console.Write("Podaj nazwisko: ");
+            o.nazwisko = Convert.ToString(Console.ReadLine());
+            Console.Write("Podaj rok urodzenia: ");
+            o.rok_urodzenia = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nImie: {0} \nNazwisko: {1} \nRok urodzenia: {2}",o.imie,o.nazwisko,o.rok_urodzenia);
+            Console.ReadKey(true);
+        }
+    }
+}
