3d7b4f29d9c3e732d335542a56a40abc3c32a428
pleibrandt <leibrandt16@gmail.com>
2025-05-19 22:39:49 +0200

    zad 11/t6

--- Zad_11_tablice_wlasne/Program.cs
+++ Zad_11_tablice_wlasne/Program.cs
@@ -10,14 +10,27 @@ namespace Zad_11_tablice_wlasne
     {
         static void Main(string[] args)
         {
-            int n;
+            int n,m,a=0;
             Console.Write("Podaj liczbe naturalna: ");
             n = Convert.ToInt32(Console.ReadLine());
             Console.Write("{0} w systemie osemkowym wynosi: ",n);
+            m = n;
+            while (m >= 1)
+            {
+                m = m / 8;
+                a++;
+            }
+            int[] tab = new int[a];
+            int j=0;
             while (n>=1)
             {
-                Console.Write("{0}", n % 8);
+                tab[j] = n % 8;
                 n = n / 8;
+                j++;
+            }
+            for(int i=a-1; i>=0; i--)
+            {
+                Console.Write("{0}", tab[i]);
             }
             Console.ReadKey(true);
         }
