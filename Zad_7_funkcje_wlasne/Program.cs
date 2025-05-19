1b07c6fe080f4bf5d544f5a1a36cf10fc82aa680
pleibrandt <leibrandt16@gmail.com>
2025-05-19 23:00:48 +0200

    zad 7/t7

---
+++ Zad_7_funkcje_wlasne/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad_7_funkcje_wlasne
+{
+    class Program
+    {
+        static void pole_deltoidu(double p, double q)
+        {
+            Console.WriteLine("Pole deltoidu wynosi: {0}",p*q/2);
+        }
+        static void Main(string[] args)
+        {
+            double p, q;
+            Console.Write("Podaj p: ");
+            p = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Podaj q: ");
+            q = Convert.ToInt32(Console.ReadLine());
+
+            pole_deltoidu(p, q);
+            Console.ReadKey(true);
+        }
+    }
+}
