c1200a935442c18e1f6ca15b7e19e6c66c4c375d
pleibrandt <leibrandt16@gmail.com>
2025-05-21 21:15:12 +0200

    zad 9/t7

---
+++ Zad_9_funkcje_wlasne/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad_9_funkcje_wlasne
+{
+    class Program
+    {
+        static void delta(double a, double b, double c)
+        {
+            if (b * b - 4 * a * c > 0)
+                Console.Write("Funkcja ma 2 pierwiastki");
+            else if (b * b - 4 * a * c == 0)
+                Console.Write("Funkcja ma 1 pierwiastek");
+            else
+                Console.Write("Funkcja ma 0 pierwiastków");
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Podaj a: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Podaj b: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Podaj c: ");
+            double c = Convert.ToDouble(Console.ReadLine());
+
+            delta(a, b, c);
+            Console.ReadKey(true);
+        }
+    }
+}
