7bd93af34805c3eff5d5e57624461b7b610c9cf9
pleibrandt <leibrandt16@gmail.com>
2025-05-21 21:40:59 +0200

    zad 13/t7

---
+++ Zad_13_dunkcje_wlasne/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad_13_dunkcje_wlasne
+{
+    class Program
+    {
+        static int czy_kw(int a)
+        {
+            int b = 1;
+            while(a>=b*b)
+            {
+                if (a == b * b)
+                    return 1;
+                b++;
+            }
+            return 0;
+        }
+        static void Main(string[] args)
+        {
+            int a;
+            Console.Write("Podaj liczbe: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("{0}",czy_kw(a));
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ Zad_13_dunkcje_wlasne/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("Zad_13_dunkcje_wlasne")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Zad_13_dunkcje_wlasne")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("984b3cb3-e704-4414-9f8c-1b883860b04b")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Poprawka
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
