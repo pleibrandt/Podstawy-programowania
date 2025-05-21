fc95299ec58e47aac68e9c64d0e97f312984d532
pleibrandt <leibrandt16@gmail.com>
2025-05-21 21:35:45 +0200

    zad 12/t7

---
+++ Zad_12_funkcje_wlasne/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad_12_funkcje_wlasne
+{
+    class Program
+    {
+        static int dec_to_oct(int n)
+        {
+            int m = n, a=0;
+            int liczba = 0, dzies = 1;
+            while (m >= 1)
+            {
+                m = m / 8;
+                a++;
+            }
+            int[] tab = new int[a];
+            int j = 0;
+            while (n >= 1)
+            {
+                tab[j] = n % 8;
+                n = n / 8;
+                j++;
+            }
+            for (int i = 0; i <a; i++)
+            {
+                liczba = liczba + tab[i] * dzies;
+                dzies = dzies * 10;
+            }
+            return liczba;
+        }
+        static void Main(string[] args)
+        {
+            int n;
+            Console.Write("Podaj liczbe: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("{0}", dec_to_oct(n));
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ Zad_12_funkcje_wlasne/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("Zad_12_funkcje_wlasne")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Zad_12_funkcje_wlasne")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("9e64326d-eac8-40c5-b1aa-aab261af55b0")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Poprawka
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
